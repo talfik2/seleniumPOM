37ed486a25b2cf92021ff1b3470c07769b2123df
talfik2 <emregondermez@gmail.com>
2022-02-05 22:14:28 +0100

    05.02.2022

--- src/test/java/tests/day17/C02_PropertiesIlkClass.java
+++ src/test/java/tests/day17/C02_PropertiesIlkClass.java
@@ -10,23 +10,26 @@ import utilities.Driver;
 public class C02_PropertiesIlkClass {
 
     @Test
-    public void positiveLoginTest() {
+    public void positiveLoginTest() throws InterruptedException {
         // Hotel my camp sitesine positive login testini POM'a tam
         // uygun olarak yapiniz
         //		 https://www. hotelmycamp.com/ adresine git
         //		login butonuna bas
         //	test data username: manager ,
         //	test data password : Manager1!
-        //	Degerleri girildiginde sayfaya girilemedigini test et
+        //	Degerleri girildiginde sayfaya girilebildigini test et
         Driver.getDriver().get(ConfigReader.getProperty("HMCUrl"));
         HotelMyCampPage hotelMyCampPage = new HotelMyCampPage();
         hotelMyCampPage.ilkLoginButton.click();
         hotelMyCampPage.usernameBox
-        .sendKeys(ConfigReader.getProperty("HMCValidUsername"),Keys.TAB,"HMCValidPassword");
+        .sendKeys(ConfigReader.getProperty("HMCValidUsername"),
+                Keys.TAB,ConfigReader.getProperty("HMCValidPassword"));
 
         hotelMyCampPage.loginButton.click();
 
         Assert.assertTrue(hotelMyCampPage.managerButton.isEnabled());
 
+        Thread.sleep(3000);
+        Driver.closeDriver();
     }
 }
--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -3,6 +3,10 @@ package utilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.opera.OperaDriver;
+import org.openqa.selenium.safari.SafariDriver;
 
 import java.time.Duration;
 
@@ -14,7 +18,7 @@ public class Driver {
 
 
     public static WebDriver getDriver() {
-        WebDriverManager.chromedriver().setup();
+
 
         // Driver.getDriver method'u her calistiginda
         // driver= new ChromeDriver(); komutundan oturu yeni bir driver olusturuyor
@@ -23,7 +27,28 @@ public class Driver {
         // bunun icin driver= new ChromeDriver(); satiri bir if blogu icine alacagiz
 
         if (driver==null) {
-            driver = new ChromeDriver();
+           switch (ConfigReader.getProperty("browser")){
+               case "chorome":
+                   WebDriverManager.chromedriver().setup();
+                   driver = new ChromeDriver();
+                   break;
+               case "opera":
+                   WebDriverManager.operadriver().setup();
+                   driver = new OperaDriver();
+                   break;
+               case "firefox":
+                   WebDriverManager.firefoxdriver().setup();
+                   driver = new FirefoxDriver();
+                   break;
+               case "safari":
+                   WebDriverManager.safaridriver().setup();
+                   driver = new SafariDriver();
+                   break;
+               case "edge":
+                   WebDriverManager.edgedriver().setup();
+                   driver = new EdgeDriver();
+                   break;
+           }
         }
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
