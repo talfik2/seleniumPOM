440133b0e28810f8dc1f4c4c10d7b76c217073e2
talfik2 <emregondermez@gmail.com>
2022-02-05 16:52:18 +0100

    05.02.2022

--- src/test/java/utilities/ConfigReader.java
+++ src/test/java/utilities/ConfigReader.java
@@ -1,5 +1,20 @@
 package utilities;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Properties;
+
 public class ConfigReader {
 
+    public static Properties properties;
+
+    static {
+        String path="src/configuration.properties";
+        try {
+            FileInputStream fis = new FileInputStream(path);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
