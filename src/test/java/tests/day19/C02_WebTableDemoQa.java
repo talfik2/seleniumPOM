c203ed6eb5a3788c37c3d4e5c32e9179f864a665
talfik2 <emregondermez@gmail.com>
2022-02-07 21:08:22 +0100

    07.02.2022

--- src/test/java/tests/day19/C02_WebTableDemoQa.java
+++ src/test/java/tests/day19/C02_WebTableDemoQa.java
@@ -21,24 +21,45 @@ public class C02_WebTableDemoQa {
 //10. Page sayfasinda bir method olusturun, Test sayfasindan satir ve sutun sayisini
 //girdigimde bana datayi yazdirsin
 
-    DemoqaPage demoqaPage;
     @Test
-    public void test01() {
+    public void demoqaTest(){
+        //  1. “https://demoqa.com/webtables” sayfasina gidin
         Driver.getDriver().get(ConfigReader.getProperty("DemoqaUrl"));
-        demoqaPage = new DemoqaPage();
-//2. Headers da bulunan department isimlerini yazdirin
-        System.out.println(demoqaPage.headers.getText());
 
-//3. 3.sutunun basligini yazdirin
-        System.out.println("3. sutunun basligi --> "+demoqaPage.basliklarListesi.get(2).getText());
+        DemoqaPage demoqaPage=new DemoqaPage();
+        //  2. Headers da bulunan department isimlerini yazdirin
+        //  normlde //thead//th olurdu ancak bu tablo class isimleri ile organize edilmis
+        System.out.println("baslik satiri elementi : "+demoqaPage.baslikSatiriElementi.getText());
 
-        System.out.println("\n<<=======>>");
+        // ikinci yontem olarak basliklari bir listeye koyabilirim
+        // basliklarWebelementiListesi
 
-        System.out.println(demoqaPage.tumListe.getText());
+        //  3. 3.sutunun basligini yazdirin
+
+        System.out.println("3.sutun basligi : "+demoqaPage.basliklarWebelementiListesi.get(2).getText());
+
+        //  4. Tablodaki tum datalari yazdirin
+        System.out.println("===========Tum body==========");
+        System.out.println(demoqaPage.bodyTekWebelement.getText());
+        //  5. Tabloda kac cell (data) oldugunu yazdirin
+        System.out.println("Tablodaki data sayisi : "+demoqaPage.tumDataWebelementList.size());
+        //  6. Tablodaki satir sayisini yazdirin
+        System.out.println("Tum satir sayisi : "+demoqaPage.tumSatirlarWebelementList.size());
+        //  7. Tablodaki sutun sayisini yazdirin
+        //     farkli yollardan hesaplanabilir ama biz hucre sayisi / satir sayisi yapalim
+        System.out.println("tablodaki sutun sayisi : " +
+                (demoqaPage.tumDataWebelementList.size()/demoqaPage.tumSatirlarWebelementList.size()));
+        //  8. Tablodaki 3.kolonu yazdirin
+        demoqaPage.sutunYazdir(2);
+
+
+        //  9. Tabloda "First Name" i Kierra olan kisinin Salary'sini yazdirin
+        demoqaPage.ismeGoreMaasYazdir("Alden");
+        //10. Page sayfasinda bir method olusturun, Test sayfasindan satir ve sutun sayisini girdigimde bana datayi yazdirsin
+
+        demoqaPage.hucreYazdir(3,2);
 
         Driver.closeDriver();
-
-
     }
 
 
