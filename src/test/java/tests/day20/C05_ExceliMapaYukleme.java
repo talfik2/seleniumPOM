5b490d25b042ac613942591ece03137cd72f5a0b
talfik2 <emregondermez@gmail.com>
2022-02-08 23:49:57 +0100

    08.02.2022

---
+++ src/test/java/tests/day20/C05_ExceliMapaYukleme.java
@@ -0,0 +1,13 @@
+package tests.day20;
+
+import org.testng.annotations.Test;
+
+public class C05_ExceliMapaYukleme {
+
+    @Test
+    public void test01() {
+
+        String path ="src/test/java/Resources/ulkeler.xlsx";
+
+    }
+}
