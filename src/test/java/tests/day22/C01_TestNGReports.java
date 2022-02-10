d94a9661091691f42d480ac681c3d471cf67ac67
talfik2 <emregondermez@gmail.com>
2022-02-10 19:46:55 +0100

    09.02.2022

--- src/test/java/tests/day22/C01_TestNGReports.java
+++ src/test/java/tests/day22/C01_TestNGReports.java
@@ -14,14 +14,19 @@ public class C01_TestNGReports extends TestBaseRapor {
 
     @Test
     public void windowHandlereusableTest() throws IOException {
+      extentTest=extentReports.createTest("Window Handle","title ile 2.sayfaya gecis yapildi");
         Driver.getDriver().get("https://the-internet.herokuapp.com/windows");
+        extentTest.info("herokuapp sayfasina gidildi");
         Driver.getDriver().findElement(By.linkText("Click Here")).click();
+        extentTest.info("ÿeni window linkine tiklandi");
         ReusableMethods.switchToWindow("New Window");
 
         String expectedTitle="New Window";
+        extentTest.info("Reusable method ile 2.sayfaya gecildi");
         String actualTitle=Driver.getDriver().getTitle();
 
         Assert.assertEquals(actualTitle,expectedTitle);
+        extentTest.pass("Title'in beklenen deger ile ayni oldugu test edildi");
         ReusableMethods.getScreenshot("WindowSwitch");
         Driver.closeDriver();
     }
